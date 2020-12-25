217e578018d03363bb66e373fac9c954ceb89ec7
weileifrank <1138289316@qq.com>
2020-12-25 12:44:12 +0800

    udapte

---
+++ gradle打包/wallet打包命令/run.sh
@@ -0,0 +1,4 @@
+java -jar  walle-cli-all.jar batch -f  channel  ./app/build/outputs/apk/dashixiong/release/clean_dashixiong_dashixiong_release_v1.00_20201225.apk  apkdir
+
+
+java -jar walle-cli-all.jar show apkdir/clean_dashixiong_dashixiong_release_v1.00_20201225_toutiao1.apk
